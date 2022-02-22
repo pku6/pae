4f9de6d3aad2159dd17fe5dfaac10933d36c29a1
PKU6 <pku6@protonmail.com>
2022-02-22 20:40:04 +0800

    update

--- renderer.js
+++ renderer.js
@@ -1,11 +1,46 @@
 const warn = document.createElement('div')
-const textarea = document.createElement('textarea')
+const configEle = document.createElement('div')
+const courseTitleInput = document.createElement('input')
+const courseNumberInput = document.createElement('input')
+const courseDepartmentInput = document.createElement('input')
+const studentIdInput = document.createElement('input')
+const passwordInput = document.createElement('input')
+const tusernameInput = document.createElement('input')
+const tpasswordInput = document.createElement('input')
+const refreshIntervalInput = document.createElement('input')
 const button = document.createElement('button')
-const out = document.createElement('div')
+const out = document.createElement('pre')
+function createNamedStretchedElement(name, content) {
+    const element = document.createElement('div')
+    const nameEle = document.createElement('div')
+    element.style.display = 'grid'
+    element.style.gridTemplateColumns = '1fr'
+    element.style.margin = 'var(--length-gap) 0'
+    nameEle.style.fontSize = 'var(--length-font-code)'
+    nameEle.textContent = name
+    element.append(nameEle)
+    element.append(content)
+    return element
+}
 document.body.append(
-    textarea,
+    configEle,
     button
 )
+configEle.append(
+    createNamedStretchedElement('Course Title', courseTitleInput),
+    createNamedStretchedElement('Course Number (optional)', courseNumberInput),
+    createNamedStretchedElement('Course Department (optional)', courseDepartmentInput),
+    createNamedStretchedElement('Student Id', studentIdInput),
+    createNamedStretchedElement('Password', passwordInput),
+    createNamedStretchedElement('TTShiTu Username', tusernameInput),
+    createNamedStretchedElement('TTShiTu Password', tpasswordInput),
+    createNamedStretchedElement('Refresh Interval', refreshIntervalInput)
+)
+courseNumberInput.type = 'number'
+passwordInput.type = 'password'
+tpasswordInput.type = 'password'
+refreshIntervalInput.step = 'any'
+refreshIntervalInput.type = 'number'
 warn.classList.add('warn')
 function alert(string) {
     warn.textContent = string
@@ -21,13 +56,29 @@ const {
     ttshitu,
     refreshInterval
 } = JSON.parse(electronAPI.loadConfig())
-textarea.value = JSON.stringify({
-    courses,
-    studentId,
-    password,
-    ttshitu,
-    refreshInterval
-}, undefined, 4)
+if (courses.length > 0) {
+    const {title, number, department} = courses[0]
+    courseTitleInput.value = title
+    if (number !== undefined) {
+        courseNumberInput.value = number.toString()
+    }
+    if (department !== undefined) {
+        courseDepartmentInput.value = department
+    }
+}
+if (studentId !== '1*000*****') {
+    studentIdInput.value = studentId
+}
+if (password !== '********') {
+    passwordInput.value = password
+}
+if (ttshitu.username !== '********') {
+    tusernameInput.value = ttshitu.username
+}
+if (ttshitu.password !== '********') {
+    tpasswordInput.value = ttshitu.password
+}
+refreshIntervalInput.value = refreshInterval.toString()
 button.textContent = 'Start'
 button.addEventListener('click', () => {
     if (button.textContent === 'Pause') {
@@ -41,49 +92,41 @@ button.addEventListener('click', () => {
         electronAPI.continue()
         return
     }
+    const course = {
+        title: courseTitleInput.value
+    }
+    const courseNumber = Number(courseNumberInput.value)
+    const courseDepartment = courseDepartmentInput.value
+    if (isFinite(courseNumber) && courseNumber > 0) {
+        course.number = courseNumber
+    }
+    if (courseDepartment.length > 0) {
+        course.department = courseDepartment
+    }
+    const refreshInterval = Number(refreshIntervalInput.value)
+    if (!isFinite(refreshInterval)) {
+        alert('Invalid refresh interval!')
+        return
+    }
+    if (refreshInterval < .5) {
+        alert('Too small refresh interval!')
+        return
+    }
+    button.textContent = 'Pause'
+    configEle.replaceWith(out)
+    electronAPI.saveConfig(JSON.stringify({
+        courses: [
+            course
+        ],
+        studentId: studentIdInput.value,
+        password: passwordInput.value,
+        ttshitu: {
+            username: tusernameInput.value,
+            password: tpasswordInput.value
+        },
+        refreshInterval
+    }))
     try {
-        const {
-            courses,
-            studentId,
-            password,
-            ttshitu: {
-                username,
-                password: tpassword
-            },
-            refreshInterval
-        } = JSON.parse(textarea.value)
-        if (
-            !Array.isArray(courses)
-            || typeof studentId !== 'string'
-            || typeof password !== 'string'
-            || typeof username !== 'string'
-            || typeof tpassword !== 'string'
-            || typeof refreshInterval !== 'number'
-            || !isFinite(refreshInterval)
-        ) {
-            throw new Error()
-        }
-        for (const {title} of courses) {
-            if (typeof title !== 'string') {
-                throw new Error()
-            }
-        }
-        if (refreshInterval < .5) {
-            alert('Too small interval!')
-            return
-        }
-        button.textContent = 'Pause'
-        textarea.replaceWith(out)
-        electronAPI.saveConfig(JSON.stringify({
-            courses,
-            studentId,
-            password,
-            ttshitu: {
-                username,
-                password: tpassword
-            },
-            refreshInterval
-        }))
     } catch (err) {
         alert('Wrong format!')
         return
