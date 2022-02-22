9020522c54ccc048ae4807a192efbb70c3940dc7
PKU6 <pku6@protonmail.com>
2022-02-22 18:30:37 +0800

    update

--- dist/main.js
+++ dist/main.js
@@ -5,8 +5,9 @@ const electron_1 = require("electron");
 const clit_1 = require("./clit");
 const init_1 = require("./init");
 const mod_1 = require("./mod");
+let currentWindow;
 function createWindow() {
-    const mainWindow = new electron_1.BrowserWindow({
+    const mainWindow = currentWindow = new electron_1.BrowserWindow({
         autoHideMenuBar: true,
         webPreferences: {
             preload: (0, path_1.join)(__dirname, '../dist/preload.js')
@@ -17,19 +18,32 @@ function createWindow() {
     });
     mainWindow.loadFile((0, path_1.join)(__dirname, '../index.html'));
 }
-let started = false;
-electron_1.ipcMain.on('save-config', (e, string) => {
-    if (started) {
-        return;
-    }
-    started = true;
-    Object.assign(init_1.config, JSON.parse(string));
-    (0, init_1.saveConfig)();
-    (0, mod_1.main)();
-});
-electron_1.app.whenReady().then(() => {
-    createWindow();
-});
-electron_1.app.on('window-all-closed', function () {
+if (!electron_1.app.requestSingleInstanceLock()) {
     electron_1.app.quit();
-});
+}
+else {
+    electron_1.app.on('second-instance', () => {
+        if (currentWindow !== undefined) {
+            if (currentWindow.isMinimized()) {
+                currentWindow.restore();
+            }
+            currentWindow.focus();
+        }
+    });
+    let started = false;
+    electron_1.ipcMain.on('save-config', (e, string) => {
+        if (started) {
+            return;
+        }
+        started = true;
+        Object.assign(init_1.config, JSON.parse(string));
+        (0, init_1.saveConfig)();
+        (0, mod_1.main)();
+    });
+    electron_1.app.whenReady().then(() => {
+        createWindow();
+    });
+    electron_1.app.on('window-all-closed', function () {
+        electron_1.app.quit();
+    });
+}
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import {app, BrowserWindow, ipcMain} from 'electron'
 import {emitter} from './clit'
 import {config, saveConfig} from './init'
 import {main} from './mod'
+let currentWindow: BrowserWindow | undefined
 function createWindow() {
-    const mainWindow = new BrowserWindow({
+    const mainWindow = currentWindow = new BrowserWindow({
         autoHideMenuBar: true,
         webPreferences: {
             preload: join(__dirname, '../dist/preload.js')
@@ -15,19 +16,31 @@ function createWindow() {
     })
     mainWindow.loadFile(join(__dirname, '../index.html'))
 }
-let started = false
-ipcMain.on('save-config', (e, string: string) => {
-    if (started) {
-        return
-    }
-    started = true
-    Object.assign(config, JSON.parse(string))
-    saveConfig()
-    main()
-})
-app.whenReady().then(() => {
-    createWindow()
-})
-app.on('window-all-closed', function () {
+if (!app.requestSingleInstanceLock()) {
     app.quit()
-})
+} else {
+    app.on('second-instance', () => {
+        if (currentWindow !== undefined) {
+            if (currentWindow.isMinimized()) {
+                currentWindow.restore()
+            }
+            currentWindow.focus()
+        }
+    })
+    let started = false
+    ipcMain.on('save-config', (e, string: string) => {
+        if (started) {
+            return
+        }
+        started = true
+        Object.assign(config, JSON.parse(string))
+        saveConfig()
+        main()
+    })
+    app.whenReady().then(() => {
+        createWindow()
+    })
+    app.on('window-all-closed', function () {
+        app.quit()
+    })
+}
